039216e5cdff31cf7f1bebadf21de0476b4423f7
Alex <olexandrvorona@gmail.com>
2026-02-10 10:43:31 +0100

    Update testimonials section description copy

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -6,8 +6,8 @@ import { cn } from '../../utils/cn';
 const Testimonials = () => {
     return (
         <section id='testimonials' className={cn(styles, 'testimonials')}>
-            <h3 className={cn(styles, 'testimonials__title')}>Recommendations</h3>
-            <p className={cn(styles, 'testimonials__description')}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores molestias perferendis explicabo, corrupti tempora maiores quaerat odit voluptas recusandae totam doloribus.</p>
+            <h3 className={cn(styles, 'testimonials__title')}>Testimonials</h3>
+            <p className={cn(styles, 'testimonials__description')}>Trusted recommendations from founders and engineering leaders I’ve collaborated with on high-impact products, long-term delivery, and complex software initiatives.</p>
             <ClientsCarousel />
           </section>
     )

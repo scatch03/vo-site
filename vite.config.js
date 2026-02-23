5076adda06a0dac8f7f54133eb13c6e102ebf616
Alex <olexandrvorona@gmail.com>
2026-02-23 09:39:21 +0100

    Small fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   server: {
     allowedHosts: ["localhost", "oleksandrvorona.tech", "www.oleksandrvorona.tech"],
   },

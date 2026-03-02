2f4e9cbd74396492e34909300b3c1b9bafe0c48a
Alex <olexandrvorona@gmail.com>
2026-03-02 17:32:25 +0100

    Update project logos, links, and responsive layout

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
-import ad from '../../assets/ad.png';
-import ex from '../../assets/ex.png';
-import dsp from '../../assets/dsp.png';    
+import consystemLogo from '../../assets/consystem-logo.svg';
+import unativeLogo from '../../assets/unative-logo.svg';
+import epomLogo from '../../assets/epom-logo.svg';
 import styles from './Projects.module.scss';
 import { cn } from '../../utils/cn';
 
@@ -9,9 +9,15 @@ import { cn } from '../../utils/cn';
 const Projects = () => {
     return (
         <section className={cn(styles, 'projects')}>
-            <img className={cn(styles, 'projects__image--ad')} src={ad} alt='Ad Server Logo' />
-            <img className={cn(styles, 'projects__image--dsp')} src={dsp} alt='DSP Platform Logo' />
-            <img className={cn(styles, 'projects__image--exchange')} src={ex} alt='Exchange Platform Logo' />
+            <a href='https://epom.com' target='_blank' rel='noopener noreferrer' aria-label='Open Epom website'>
+              <img className={cn(styles, 'projects__image')} src={epomLogo} alt='Epom Logo' />
+            </a>
+            <a href='https://unative.com' target='_blank' rel='noopener noreferrer' aria-label='Open Unative website'>
+              <img className={cn(styles, 'projects__image')} src={unativeLogo} alt='Unative Logo' />
+            </a>
+            <a href='https://consysteam.com' target='_blank' rel='noopener noreferrer' aria-label='Open Consysteam website'>
+              <img className={cn(styles, 'projects__image')} src={consystemLogo} alt='Consystem Logo' title='CONSYSTEAM' />
+            </a>
           </section>
     )
 }   
